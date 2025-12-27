c261a7d5f4fda608058f81724bf465f060bb17ae
kjorg50 <kylewjorgensen@gmail.com>
2025-12-27 13:36:44 -0800

    Day01 part 2 complete

--- kotlin/01/day01.kt
+++ kotlin/01/day01.kt
@@ -3,6 +3,7 @@
 // https://adventofcode.com/2025/day/1
 
 import java.io.File
+import kotlin.math.abs
 
 fun part1(filename: String) {
 
@@ -10,7 +11,6 @@ fun part1(filename: String) {
     var current = 50
 
     File(filename).forEachLine {
-        // access each line with `it` (type String)
         val firstChar = it.first()
         val number = it.substring(1).toInt()
         when (firstChar) {
@@ -32,15 +32,48 @@ fun part1(filename: String) {
 }
 
 fun part2(filename: String) {
+    var result = 0
+    var current = 50
 
     File(filename).forEachLine {
-        // access each line with `it` (type String)
-    }
+        val firstChar = it.first()
+        val number = it.substring(1).toInt()
 
+        val wraps = number / 100
+        result += wraps
+//        println("Adding wraps: $wraps, total now: $result")
+
+        val remaining = number % 100
+
+        if (remaining > 0) {
+            val start = current
+            val end: Int
+
+            if (firstChar == 'R') {
+                end = (start + remaining) % 100
+                if (start != 0 && end < start || end == 0) {
+                    result++
+//                    println("Additional wrap from $start to $end, new total is $result" )
+                }
+            } else {
+                end = (start - remaining + 100) % 100
+                if (start != 0 && end > start || end == 0) {
+                    result++
+//                    println("Additional wrap from $start to $end, new total is $result" )
+                }
+            }
+            current = end
+        } else {
+            // exactly divisible by 100
+            // current remains the same, no additional wrap
+            current = current
+        }
+    }
+    println("Result: $result")
 }
 
 fun main(args: Array<String>) {
-    part1("input")
-//    part2("input")
+//    part1("input")
+    part2("input")
 }
 
